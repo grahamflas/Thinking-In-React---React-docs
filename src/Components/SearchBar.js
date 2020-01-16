d6861ccb5c9d9f2624b0c7d40a86137bfaa7a36e
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-16 10:42:33 -0500

    Import data in index.js, pass to App as props.

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+  render(){
+    return (
+      <form>
+        <input type="text" placeholder="Search..."/>
+        <p>
+          <input type="checkbox"/>
+          Only show products that are in-stock
+        </p>
+      </form>
+    )
+  }
+}
+
+export default SearchBar
