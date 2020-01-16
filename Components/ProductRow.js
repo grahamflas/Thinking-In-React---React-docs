6b0a7a4d429f0e32012abe2aee070a1a2697e605
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-16 10:29:41 -0500

    Render product name and price in table, name === red if not in stock

--- Components/ProductRow.js
+++ Components/ProductRow.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 class ProductRow extends React.Component{
   render(){
+    const product = this.props.product
+    const name = product.stocked ? product.name : (
+      <span style={ {color: 'red'} }>{product.name}</span>
+    )
+
+
     return(
-      <div>
-        Product Row
-      </div>
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
     )
   }
 }
