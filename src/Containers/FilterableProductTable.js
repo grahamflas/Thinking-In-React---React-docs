e2d3ba1f025b65aa21def77c43f6cbd9593c944e
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-16 11:59:40 -0500

    Add state for filter text and instock only

--- src/Containers/FilterableProductTable.js
+++ src/Containers/FilterableProductTable.js
@@ -3,11 +3,25 @@ import SearchBar from '../Components/SearchBar';
 import ProductTable from './ProductTable';
 
 class FilterableProductTable extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      filterText: "",
+      inStockOnly: false
+    }
+  }
   render(){
     return(
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products}/>
+        <SearchBar 
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+        <ProductTable 
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     )
   }
